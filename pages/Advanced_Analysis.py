7ec4fe3a555e686c998b6dd3c603a87db253a99b
‘Rob <rob@calcifireconsulting.com>
2023-09-13 00:16:26 -0400

    modified:   .streamlit/config.toml 	modified:   pages/Advanced_Analysis.py 	modified:   pages/Image_Generation.py

--- pages/Advanced_Analysis.py
+++ pages/Advanced_Analysis.py
@@ -18,7 +18,7 @@ from chat2plot.chat2plot import Chat2Vega
 
 sys.path.append("../../")
 
-############ Set up page configuration and favicon ################
+# Set up page configuration and favicon
 basewidth = 600
 favicon = Image.open("static/alpha.png")
 wpercent = (basewidth / float(favicon.size[0]))
@@ -29,39 +29,31 @@ st.set_page_config(
     page_title="Alpha-Advanced-Analysis",
     page_icon=img,
     layout="wide",
-    initial_sidebar_state="expanded")
+    initial_sidebar_state="expanded"
+)
 
-#####################################################################
-
-
-########### Setup api Keys ###################################
 # Retrieve secrets
 ZAPIER_CLIENT_SECRET = st.secrets["ZAPIER_CLIENT_SECRET"]
 ZAPIER_CLIENT_ID = st.secrets["ZAPIER_CLIENT_ID"]
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-###############################################################
 
-
-############# USER Authentication Creation ###########################
+# User Authentication Creation
 users = db.fetch_all_users()
 usernames = [user["key"] for user in users]
 names = [user["name"] for user in users]
 hashed_passwords = [user["password"] for user in users]
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "AlphaChat", "abcdef", cookie_expiry_days= 1 )
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "AlphaChat", "abcdef", cookie_expiry_days=1)
 name, authentication_status, username = authenticator.login("Login", "sidebar")
-######################################################################
 
-
-############### Authentication Status Conditionals ################
+# Authentication Status Conditionals
 if st.session_state["authentication_status"]:
     st.write(f'Welcome *{st.session_state["name"]}*')
 elif st.session_state["authentication_status"] is False:
     st.sidebar.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
-    st.sidebar.warning('Please enter your username and password') 
-#####################################################################
+    st.sidebar.warning('Please enter your username and password')
 
-############ Initialize Logger#####################
+# Initialize Logger
 def initialize_logger():
     logger = logging.getLogger("root")
     handler = logging.StreamHandler(sys.stdout)
@@ -69,116 +61,142 @@ def initialize_logger():
     logger.handlers = [handler]
     return True
 
-
 if "logger" not in st.session_state:
     st.session_state["logger"] = initialize_logger()
-#######################################################
 
-api_key = openai.api_key = st.secrets["OPENAI_API_KEY"]
+# API Key setup
+api_key = openai.api_key = OPENAI_API_KEY
 csv_file = st.sidebar.file_uploader("Step1: Upload CSV/XLSX/XLS File", type={"csv"})
+if authentication_status:
+    if api_key and csv_file:
+        df = pd.read_csv(csv_file)
+        st.write("Tabulated Data:")
+        st.write(df.head())
 
-if api_key and csv_file:
-    df = pd.read_csv(csv_file)
-    st.write("Tabulated Data:")
-    st.write(df.head())
+        if "generated" not in st.session_state:
+            st.session_state["generated"] = []
 
-    if "generated" not in st.session_state:
-        st.session_state["generated"] = []
+        if "past" not in st.session_state:
+            st.session_state["past"] = []
 
-    if "past" not in st.session_state:
-        st.session_state["past"] = []
+        st.text(f'Chat History For {st.session_state["name"]}')
 
-    st.text(f'Chat History For {st.session_state["name"]}')
+        def initialize_c2p():
+            st.session_state["chat"] = chat2plot(
+                df,
+                st.session_state["chart_format"],
+                verbose=True,
+                description_strategy="head",
+            )
 
-    def initialize_c2p():
-        st.session_state["chat"] = chat2plot(
-            df,
-            st.session_state["chart_format"],
-            verbose=True,
-            description_strategy="head",
-        )
+        def reset_history():
+            initialize_c2p()
+            st.session_state["generated"] = []
+            st.session_state["past"] = []
 
-    def reset_history():
-        initialize_c2p()
-        st.session_state["generated"] = []
-        st.session_state["past"] = []
+        with st.sidebar:
+            chart_format = st.selectbox(
+                "Chart format",
+                ("simple", "vega"),
+                key="chart_format",
+                index=0,
+                on_change=initialize_c2p,
+            )
 
-    with st.sidebar:
-        chart_format = st.selectbox(
-            "Chart format",
-            ("simple", "vega"),
-            key="chart_format",
-            index=0,
-            on_change=initialize_c2p,
-        )
+            st.button("Reset conversation history", on_click=reset_history)
+            authenticator.logout('Logout', 'sidebar',) 
+        if "chat" not in st.session_state:
+            initialize_c2p()
 
-        st.button("Reset conversation history", on_click=reset_history)
-   
-    if "chat" not in st.session_state:
-        initialize_c2p()
+        c2p = st.session_state["chat"]
 
-    c2p = st.session_state["chat"]
+        chat_container = st.container()
+        input_container = st.container()
+        st.sidebar.write("[Terms of Service](https://docs.google.com/document/e/2PACX-1vRsnJ_liUiUnyrysB380Thgcu-jBRZ57YQgvXusDVO11F4QGe49sea5iYV1SJuaSKDbg9D6OhXDqPMr/pub)") 
+        st.sidebar.write("[Privacy Policy](https://docs.google.com/document/e/2PACX-1vRGFn8CTVLdRdjmNJ9DPusSmiwcjfxDKO9K8yh0cyR_Zazb0kLGqv3gEoRhKOIOWxkWTOpPtUWXyeFt/pub)")
 
-    chat_container = st.container()
-    input_container = st.container()
-    st.sidebar.write("[Terms of Service](https://docs.google.com/document/d/e/2PACX-1vRsnJ_liUiUnyrysB380Thgcu-jBRZ57YQgvXusDVO11F4QGe49sea5iYV1SJuaSKDbg9D6OhXDqPMr/pub)") 
-    st.sidebar.write("[Privacy Policy](https://docs.google.com/document/d/e/2PACX-1vRGFn8CTVLdRdjmNJ9DPusSmiwcjfxDKO9K8yh0cyR_Zazb0kLGqv3gEoRhKOIOWxkWTOpPtUWXyeFt/pub)")
-    
-    def submit(input_text):
-        submit_text = input_text
-        st.session_state["input"] = ""
-        with st.spinner(text="Waiting for Alpha's response..."):
-            try:
-                if isinstance(c2p, Chat2Vega):
-                    res = c2p(submit_text, config_only=True)
-                else:
-                    res = c2p(submit_text, config_only=False, show_plot=False)
-            except Exception:
-                res = traceback.format_exc()
-        st.session_state.past.append(submit_text)
-        st.session_state.generated.append(res)
+        def submit(input_text):
+            submit_text = input_text
+            st.session_state["input"] = ""
+            with st.spinner(text="Waiting for Alpha's response..."):
+                try:
+                    if isinstance(c2p, Chat2Vega):
+                        res = c2p(submit_text, config_only=True)
+                    else:
+                        res = c2p(submit_text, config_only=False, show_plot=False)
+                except Exception:
+                    res = traceback.format_exc()
+            st.session_state.past.append(submit_text)
+            st.session_state.generated.append(res)
 
-    def get_text():
-        if input_text := st.chat_input(f'{st.session_state["name"]}"''s Prompt'):
-            submit(input_text)
-        return input_text
+        def get_text():
+            if input_text := st.chat_input(f'{st.session_state["name"]}"''s Prompt'):
+                submit(input_text)
+            return input_text
 
-    with input_container:
-        user_input = get_text()
+        with input_container:
+            user_input = get_text()
 
-    if st.session_state["generated"]:
-        with chat_container:
-            for i in range(
-                len(st.session_state["generated"])):  # range(len(st.session_state["generated"]) - 1, -1, -1):
-                message(st.session_state["past"][i], is_user=True, avatar_style="avataaars", seed="24", key=str(i) + "_user")
+        if st.session_state["generated"]:
+            with chat_container:
+                for i in range(len(st.session_state["generated"])):
+                    message(st.session_state["past"][i], is_user=True, avatar_style="avataaars", seed="24", key=str(i) + "_user")
 
-                res = st.session_state["generated"][i]
+                    res = st.session_state["generated"][i]
 
-                if isinstance(res, str):
-                    # something went wrong
-                    st.error(res.replace("\n", "\n\n"))
-                elif res.response_type == ResponseType.SUCCESS:
-                    message(res.explanation, is_user=False, avatar_style="avataaars-neutral", seed="Aneka114", key=str(i))
+                    if isinstance(res, str):
+                        st.error(res.replace("\n", "\n\n"))
+                    elif res.response_type == ResponseType.SUCCESS:
+                        message(res.explanation, is_user=False, avatar_style="avataaars-neutral", seed="Aneka114", key=str(i))
 
-                    col1, col2 = st.columns([2, 1])
+                        col1, col2 = st.columns([2, 1])
 
-                    with col2:
-                        config = res.config
-                        if isinstance(config, BaseModel):
-                            st.code(
-                                config.json(indent=2, exclude_none=True),
-                                language="json",
-                            )
-                        else:
-                            st.code(json.dumps(config, indent=2), language="json")
-                    with col1:
-                        if isinstance(res.figure, Figure):
-                            st.plotly_chart(res.figure, use_container_width=True)
-                        else:
-                            st.vega_lite_chart(df, res.config, use_container_width=True)
-                else:
-                    st.warning(
-                        f"Failed to render chart. last message: {res.conversation_history[-1].content}",
-                        icon="⚠️",
-                    )
-                    
+                        with col2:
+                            config = res.config
+                            if isinstance(config, BaseModel):
+                                st.code(
+                                    config.json(indent=2, exclude_none=True),
+                                    language="json",
+                                )
+                            else:
+                                st.code(json.dumps(config, indent=2), language="json")
+                        with col1:
+                            if isinstance(res.figure, Figure):
+                                st.plotly_chart(res.figure, use_container_width=True)
+                            else:
+                                st.vega_lite_chart(df, res.config, use_container_width=True)
+                    else:
+                        st.warning(
+                            f"Failed to render chart. last message: {res.conversation_history[-1].content}",
+                            icon="⚠️",
+                        )
+        
+       
+else:
+        st.write("# Welcome to Alpha Playground! 👋")
+
+        #st.sidebar.success("Login and select the demo from above.")
+        st.chat_message("Hi. I'm Alpha, your friendly intelligent assistant. To get started, enter your username and password in the left sidebar.", avatar_style="avataaars-neutral", seed="Aneka114", key='intro_message_1')
+        
+        st.markdown(
+            """
+            Alpha Playground is a live demo of our app framework built specifically for
+            Intelligent Systems and Business Solutions.
+            
+            👈 Login to see some examples
+            of what Alpha can do!
+            ### Want to learn more?
+            - Check out [Calcifire Consulting](https://calcifireconsulting.com)
+            - Jump into our [documentation](https://docs.streamlit.io)
+            - Ask a question in our [community
+                forums](https://discuss.streamlit.io)
+            ### To See more complex demos Like:
+            
+            - Voice Chat (Speech to Text)
+            - Voice Commands (Keyword Implementation)
+            - Agent TTS (Text to Speech)
+            - Computer Vision
+            
+            contact us at info@calcifireconsulting.com
+        """
+        )    
--- pages/Image_Generation.py
+++ pages/Image_Generation.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import openai
-import streamlit_chat
-from PIL import Image
 import streamlit.components.v1 as components
-from langchain import OpenAI
+from PIL import Image
 import streamlit_authenticator as stauth
 import database as db
+from langchain.memory import StreamlitChatMessageHistory, ConversationBufferMemory
+import streamlit_chat 
 
-############ Set up page configuration and favicon ################
+# Set up page configuration and favicon
 basewidth = 600
 favicon = Image.open("static/alpha.png")
 wpercent = (basewidth / float(favicon.size[0]))
@@ -16,173 +16,129 @@ img = favicon.resize((basewidth, hsize), Image.LANCZOS)
 img.save('static/resized_image.png')
 st.set_page_config(
     page_title="Alpha-Assist",
-    page_icon= img,
+    page_icon=img,
     layout="wide",
-    initial_sidebar_state="expanded")
-#####################################################################
+    initial_sidebar_state="expanded"
+)
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-
-
-############# USER Authentication Creation ###########################
+# User Authentication Creation
 users = db.fetch_all_users()
 usernames = [user["key"] for user in users]
 names = [user["name"] for user in users]
 hashed_passwords = [user["password"] for user in users]
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "AlphaChat", "abcdef", cookie_expiry_days= 1 )
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "AlphaChat", "abcdef", cookie_expiry_days=1)
 name, authentication_status, username = authenticator.login("Login", "sidebar")
-######################################################################
 
-
-############### Authentication Status Conditionals ################
+# Authentication Status Conditionals
 if st.session_state["authentication_status"]:
     st.write(f'Welcome *{st.session_state["name"]}*')
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
-    st.sidebar.warning('Please enter your username and password') 
-#####################################################################
+    st.sidebar.warning('Please enter your username and password')
 
 
-#Main entry point when the user is authenticated
-if authentication_status:
-    number_of_images = st.sidebar.selectbox("Choose Number of Images",(1, 2, 3, 4))
-    image_size = st.sidebar.selectbox("Choose Image Resolution",("256x256", "512x512", "1024x1024"))
-    def generate_messages(prompt):
-        response = openai.Image.create(
-            prompt=prompt,
-            n = number_of_images,
-            size=image_size
+# Create Agent's Conversational Buffer Memory
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    chat_memory=msgs, return_messages=True, memory_key="chat_history", output_key="output"
+)
+
+
+def main():
+    # Check User Authentication Status (Main Entry point)
+    if authentication_status:
+        # Image generation function from the first message
+        def generate_messages(prompt):
+            response = openai.Image.create(
+                prompt=prompt,
+                n=number_of_images,
+                size=image_size
+            )
+
+            image_urls = []
+
+            if 'data' in response and len(response['data']) > 0:
+                for data_item in response['data']:
+                    if 'url' in data_item:
+                        image_urls.append(data_item['url'])
+
+            if image_urls:
+                return image_urls
+            else:
+                return ["Image generation failed"] * 4
+
+        # Initialize Session States
+        if "images" not in st.session_state:
+            st.session_state.images = []
+
+        number_of_images = st.sidebar.selectbox("Choose Number of Images", (1, 2, 3, 4))
+        image_size = st.sidebar.selectbox("Choose Image Resolution", ("256x256", "512x512", "1024x1024"))
+
+        # Create Tab Menu
+        ChatBot = st.container()
+        with ChatBot:
+            st.title("Chat With Alpha")
+            st.text("Intelligent chatbot with access to the web \nAsk Alpha questions or upload a file to get insights")
+
+            st.text("Chat History:")
+            for i, message in enumerate(st.session_state.images):
+                if message["role"] == "user":
+                    st.chat_message(message["content"], is_user=True, avatar_style="avataaars", seed="24")
+                else:
+                    st.chat_message(message["content"], is_user=False, avatar_style="avataaars-neutral", seed="Aneka114")
+
+        if user_content := st.chat_input("Hello, my name is Alpha. Type your questions here.", key="main_chat_input"):
+            with ChatBot:
+                st.session_state.images.append({"role": "user", "content": user_content})
+                assistant_content = generate_messages(user_content)
+                st.chat_message(user_content, is_user=True, avatar_style="avataaars", seed="24")
+
+                if assistant_content and not all(url.startswith("Image generation failed") for url in assistant_content):
+                    for url in assistant_content:
+                        if not url.startswith("Image generation failed"):
+                            st.session_state.images.append({"role": "assistant", "content": url})
+                            st.chat_message(url, is_user=False, avatar_style="avataaars-neutral", seed="Aneka114")
+                        else:
+                            st.session_state.images.append({"role": "assistant", "content": "Image generation failed. Please try again."})
+                            st.chat_message("Image generation failed. Please try again.", is_user=False, avatar_style="avataaars-neutral", seed="Aneka114")
+
+        def reset_history():
+            st.session_state["images"] = []
+            
+        st.sidebar.button("Clear Conversation", key='clear_chat_button', on_click=reset_history)
+        authenticator.logout('Logout', 'sidebar',)
+        st.sidebar.write("[Terms of Service](https://docs.google.com/document/e/2PACX-1vRsnJ_liUiUnyrysB380Thgcu-jBRZ57YQgvXusDVO11F4QGe49sea5iYV1SJuaSKDbg9D6OhXDqPMr/pub)")
+        st.sidebar.write("[Privacy Policy](https://docs.google.com/document/e/2PACX-1vRGFn8CTVLdRdjmNJ9DPusSmiwcjfxDKO9K8yh0cyR_Zazb0kLGqv3gEoRhKOIOWxkWTOpPtUWXyeFt/pub)")
+
+    else:
+        st.write("# Welcome to Alpha Playground! 👋")
+
+        st.chat_message("Hi. I'm Alpha, your friendly intelligent assistant. To get started, enter your username and password in the left sidebar.", avatar_style="avataaars-neutral", seed="Aneka114")
+
+        st.markdown(
+            """
+            Alpha Playground is a live demo of our app framework built specifically for
+            Intelligent Systems and Business Solutions.
+
+            👈 Login to see some examples
+            of what Alpha can do!
+            ### Want to learn more?
+            - Check out [Calcifire Consulting](https://calcifireconsulting.com)
+            - Jump into our [documentation](https://docs.streamlit.io)
+            - Ask a question in our [community
+                forums](https://discuss.streamlit.io)
+            ### To See more complex demos Like:
+
+            - Voice Chat (Speech to Text)
+            - Voice Commands (Keyword Implementation)
+            - Agent TTS (Text to Speech)
+            - Computer Vision
+
+            contact us at info@calcifireconsulting.com
+        """
         )
 
-        image_urls = []
-        
-        # Check if the response contains image URLs and extract them
-        if 'data' in response and len(response['data']) > 0:
-            for data_item in response['data']:
-                if 'url' in data_item:
-                    image_urls.append(data_item['url'])
 
-        if image_urls:
-            return image_urls 
-        else:
-            return ["Image generation failed"] * 4
-
-
-  
-
-    # Initialize the chat history
-    if "images" not in st.session_state:
-        st.session_state.images = []
-
-    # Title for your Streamlit app
-    st.title("Image Generation with Alpha")
-
-
-
-    st.write("Chat History:")
-    # Logout button and clear chat button
-    authenticator.logout('Logout', 'sidebar',)
-    if st.sidebar.button("Clear Conversation", key='clear_chat_button'):
-        st.session_state.images = []
-
-    # Links to terms of service and privacy policy
-    st.sidebar.write("[Terms of Service](https://docs.google.com/document/d/e/2PACX-1vRsnJ_liUiUnyrysB380Thgcu-jBRZ57YQgvXusDVO11F4QGe49sea5iYV1SJuaSKDbg9D6OhXDqPMr/pub)")
-    st.sidebar.write("[Privacy Policy](https://docs.google.com/document/d/e/2PACX-1vRGFn8CTVLdRdjmNJ9DPusSmiwcjfxDKO9K8yh0cyR_Zazb0kLGqv3gEoRhKOIOWxkWTOpPtUWXyeFt/pub)")
-   # User input and response handling
-    if user_content := st.chat_input("Type Your Image Prompt Here.", key="main_chat_input"):
-        # Append the user's message to the chat history
-        st.session_state.images.append({"role": "user", "content": user_content})
-        
-        # Generate images based on the user's input
-        assistant_content = generate_messages(user_content)
-
-        # Append the assistant's image responses to the chat history
-        if assistant_content and not all(url.startswith("Image generation failed") for url in assistant_content):
-            for url in assistant_content:
-                if not url.startswith("Image generation failed"):
-                    st.session_state.images.append({"role": "assistant", "content": url})
-                else:
-                    st.session_state.images.append({"role": "assistant", "content": "Image generation failed. Please try again."})
-        else:
-            error_message = "Image generation failed. Please try again."
-            st.session_state.images.extend([{"role": "assistant", "content": error_message}] * 4)
-
-   # ...
-
-    # Display the chat history only when there are messages
-    if st.session_state.images:
-        chat_history_container = st.container()
-        with chat_history_container:
-            for i, message in enumerate(st.session_state.images):
-                message_key = f"message_{i}"  # Generate a unique key for each message
-                if message["role"] == "user":
-                    streamlit_chat.message(
-                        message["content"],
-                        is_user=True,
-                        avatar_style="avataaars",
-                        seed="24",
-                        key=message_key  # Pass the unique key here
-                    )
-                elif message["role"] == "assistant":
-                    col1, col2, col3, col4 = st.columns(4)
-                    if isinstance(message["content"], list) and len(message["content"]) >= 4:
-                        with col1:
-                            st.image(message["content"][0], use_column_width=False)
-                        with col2:
-                            st.image(message["content"][1], use_column_width=False)
-                        with col3:
-                            st.image(message["content"][2], use_column_width=False)
-                        with col4:
-                            st.image(message["content"][3], use_column_width=False)
-                    elif isinstance(message["content"], str) and message["content"].startswith("http"):
-                        # Display a single image
-                        st.image(message["content"], use_column_width=False)
-                        # Generate a unique key for the image based on the message key
-                        image_key = f"image_{i}"
-                        st.markdown(f'<div id="{image_key}"></div>', unsafe_allow_html=True)
-                        st.markdown(f"""
-                            <script>
-                                document.getElementById("{image_key}").parentElement.parentElement.setAttribute("id", "{message_key}");
-                            </script>
-                        """, unsafe_allow_html=True)
-                    else:
-                        # Handle other message types
-                        streamlit_chat.message(
-                            message["content"],
-                            is_user=False,
-                            avatar_style="avataaars-neutral",
-                            seed="Aneka114",
-                            key=message_key  # Pass the unique key here
-                        )
-
-    # ...
-
-# If the user is not authenticated
-else:
-    # Display an introductory message and authentication instructions
-    streamlit_chat.message("Hi. I'm Alpha, your friendly intelligent assistant. To get started, enter your username and password in the left sidebar.",
-                           avatar_style="avataaars-neutral", seed="Aneka114")
-
-    st.markdown(
-        """
-        Alpha Playground is a live demo of our app framework built specifically for
-        Intelligent Systems and Business Solutions.
-        
-        👈 Login to see some examples
-        of what Alpha can do!
-        ### Want to learn more?
-        - Check out [Calcifire Consulting](https://calcifireconsulting.com)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-            forums](https://discuss.streamlit.io)
-        ### To See more complex demos Like:
-        
-        - Voice Chat (Speech to Text)
-        - Voice Commands (Keyword Implementation)
-        - Agent TTS (Text to Speech)
-        - Computer Vision
-        
-        contact us at info@calcifireconsulting.com
-    """
-    )
+if __name__ == '__main__':
+    main()
